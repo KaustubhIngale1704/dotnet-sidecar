8eed4eb2a60ec4ea018698cd44e1057c82e17cae
Kaustubh Ingale <kingale@hbkworld.com>
2025-08-25 12:24:38 +0200

    changed to .net8.0

--- src/server/Program.cs
+++ src/server/Program.cs
@@ -9,17 +9,7 @@ builder.Services.AddCors(options =>
     });
 });
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 var summaries = new[]
 {
